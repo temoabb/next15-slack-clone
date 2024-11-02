f14e37f89f45c133b5ad98d209b17a7d91e5714c
Temo Abesadze <abesadzetemo1997@gmail.com>
2024-11-03 01:58:24 +0400

    Install convex for the backend and implement OAuth with a github provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
+"use client";
+import { Button } from "@/components/ui/button";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  return <AuthScreen />;
+  const { signOut } = useAuthActions();
+
+  return (
+    <>
+      <h1>Logged in!</h1>
+      <Button onClick={() => signOut()}>Sign Out</Button>
+    </>
+  );
 }
+
+// If component is a client component ("use client") and it is a wrapper on the {children}, it does not mean children and their children need to be a client components. They can be SERVER components too.
--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -12,14 +12,20 @@ import {
 } from "@/components/ui/card";
 import { SignInFlow } from "../types";
 import { useState } from "react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 interface SignInCardProps {
   setState: (state: SignInFlow) => void;
 }
 
 export const SignInCard: React.FC<SignInCardProps> = ({ setState }) => {
+  const { signIn } = useAuthActions();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleProviderSignIn = (value: "github" | "google") => {
+    signIn(value);
+  };
 
   return (
     <Card className="w-full h-full p-8">
@@ -69,7 +75,7 @@ export const SignInCard: React.FC<SignInCardProps> = ({ setState }) => {
 
           <Button
             disabled={false}
-            onClick={() => {}}
+            onClick={() => handleProviderSignIn("github")}
             variant="outline"
             size="lg"
             className="w-full relative"
