04b49e623622d9eb45a14ef7ced767ab1190fe98
Temo Abesadze <abesadzetemo1997@gmail.com>
2024-11-13 17:44:15 +0400

    Create an user button (dropdown) component

---
+++ convex/users.ts
@@ -0,0 +1,17 @@
+import { query } from "./_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
+// import { auth } from "./auth";
+
+export const current = query({
+  args: {},
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (userId === null) {
+      return null;
+    }
+
+    // When you use a 'db' method and you 'get' something you do not need to specify a collection which you are trying to get. It has an unique id for 'users'
+    return await ctx.db.get(userId);
+  },
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { useAuthActions } from "@convex-dev/auth/react";
-
-export default function Home() {
-  const { signOut } = useAuthActions();
-
-  return (
-    <>
-      <Button onClick={() => signOut()}>Sign Out</Button>
-      Logged in!
-    </>
-  );
-}
+import UserButton from "@/features/auth/components/UserButton";
 
 // If component is a client component ("use client") and it is a wrapper on the {children}, it does not mean children and their children need to be a client components. They can be SERVER components too.
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton />
+    </div>
+  );
+}
